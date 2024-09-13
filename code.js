46a40277113b5503424a114af38491d731eb2055
MBoumedine <mireilleboumedine@gmail.com>
2024-09-13 11:13:01 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,248 @@
+const urlBase = 'http://98.80.118.246/LAMPAPI';
+const extension = 'php';
+
+let userId = 0;
+//let firstName = "";
+//let lastName = "";
+
+function doLogin()
+{
+	console.log("tried to log in");
+
+	userId = 0;
+	
+	let username = document.getElementById("username").value;
+	let password = document.getElementById("password").value;
+	//let hash = md5( password );
+	
+	document.getElementById("loginResult").innerHTML = "";
+
+	let tmp = {username:username,password:password};
+	//let tmp = {username:username,password:hash};
+	let jsonPayload = JSON.stringify( tmp );
+	
+	let url = urlBase + '/Login.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			// if readyState is 4 then the request is done, 200 means "OK"
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				let jsonObject = JSON.parse( xhr.responseText );
+				userId = jsonObject.id;
+		
+				if( userId < 1 )
+				{		
+					document.getElementById("loginResult").innerHTML = "Username/Password combination incorrect";
+					return;
+				}
+				console.log(userId);
+				console.log("test1");
+				//firstName = jsonObject.name;
+				//lastName = jsonObject.lastName;
+				//console.log("test2");
+				//saveCookie();
+	
+				window.location.href = "signup.html";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+
+}
+
+function doSignup()
+{
+	console.log("tried to sign up");
+
+	userId = 0;
+	
+	let username = document.getElementById("username").value;
+	let password = document.getElementById("password").value;
+	//let hash = md5( password );
+	
+	document.getElementById("signupResult").innerHTML = "";
+
+	let tmp = {username:username,password:password};
+	//let tmp = {username:username,password:hash};
+	let jsonPayload = JSON.stringify( tmp );
+	
+	let url = urlBase + '/Signup.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			// if readyState is 4 then the request is done, 200 means "OK"
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				//let jsonObject = JSON.parse( xhr.responseText );
+				//userId = jsonObject.id;
+		/*
+				if( userId < 1 )
+				{		
+					document.getElementById("signupResult").innerHTML = "Username already exists";
+					return;
+				}
+				*/
+				//console.log(userId);
+				//console.log("test1");
+				//firstName = jsonObject.name;
+				//lastName = jsonObject.lastName;
+				//console.log("test2");
+				//saveCookie();
+
+				window.location.href = "index.html";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("signupResult").innerHTML = err.message;
+	}
+
+}
+
+
+/*
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "login.html";
+	}
+	else
+	{
+//		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+}
+
+function doLogout()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "login.html";
+}
+*/
+// TODO: Edit functionality to work like searching contacts
+/*
+function addColor()
+{
+	let newColor = document.getElementById("colorText").value;
+	document.getElementById("colorAddResult").innerHTML = "";
+
+	let tmp = {color:newColor,userId,userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/AddColor.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("colorAddResult").innerHTML = "Color has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("colorAddResult").innerHTML = err.message;
+	}
+	
+}
+
+function searchColor()
+{
+	let srch = document.getElementById("searchText").value;
+	document.getElementById("colorSearchResult").innerHTML = "";
+	
+	let colorList = "";
+
+	let tmp = {search:srch,userId:userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/SearchColors.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
+				let jsonObject = JSON.parse( xhr.responseText );
+				
+				for( let i=0; i<jsonObject.results.length; i++ )
+				{
+					colorList += jsonObject.results[i];
+					if( i < jsonObject.results.length - 1 )
+					{
+						colorList += "<br />\r\n";
+					}
+				}
+				
+				document.getElementsByTagName("p")[0].innerHTML = colorList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("colorSearchResult").innerHTML = err.message;
+	}
+	
+}
+    */
--- signup.php
+++ signup.php
@@ -1,1 +1,67 @@
-<?php// signup.phprequire '../config/db.php';// Ensure the request method is POSTif ($_SERVER['REQUEST_METHOD'] === 'POST') {	// Get the raw JSON input$jsonData = file_get_contents("php://input");$data = json_decode($jsonData, true); // Convert the JSON input to an associative array	// Extract the fields from the decoded JSON$username = $data['username'];$password = $data['password']; // Assuming this is already hashed in your frontend$email = $data['email'];$fullName = $data['fullName'];	// Check if the username already exists$stmt = $conn->prepare("SELECT * FROM users WHERE username = :username");$stmt->execute(['username' => $username]);if ($stmt->rowCount() > 0) {echo json_encode(['status' => 'error', 'message' => 'Username already exists']);exit;}	// Insert the new user into the database$stmt = $conn->prepare("INSERT INTO users (username, password, email, fullName) VALUES (:username, :password>$stmt->execute(['username' => $username,'password' => $password,'email' => $email,'fullName' => $fullName]);echo json_encode(['status' => 'success', 'message' => 'User registered successfully']);} else {echo json_encode(['status' => 'error', 'message' => 'Invalid request method']);}?><?php
+<?php
+// signup.php
+require '../config/db.php';
+
+// Ensure the request method is POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get the raw JSON input
+    $jsonData = file_get_contents("php://input");
+    $data = json_decode($jsonData, true); // Convert the JSON input to an associative array
+
+    // Extract the fields from the decoded JSON
+    $username = $data['username'];
+    $password = $data['password']; // Assuming this is already hashed in your frontend
+    $email = $data['email'];
+    $fullName = $data['fullName'];
+
+    // Check if the username already exists
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = :username");
+    $stmt->execute(['username' => $username]);
+
+    if ($stmt->rowCount() > 0) {
+        echo json_encode(['status' => 'error', 'message' => 'Username already exists']);
+        exit;
+    }
+
+    // Insert the new user into the database
+    $stmt = $conn->prepare("INSERT INTO users (username, password, email, fullName) VALUES (:username, :password, :email, :fullName)");
+    $stmt->execute([
+        'username' => $username,
+        'password' => $password,
+        'email' => $email,
+        'fullName' => $fullName
+    ]);
+
+    echo json_encode(['status' => 'success', 'message' => 'User registered successfully']);
+} else {
+    echo json_encode(['status' => 'error', 'message' => 'Invalid request method']);
+}
+?>
+<?php
+require '../config/db.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Check if the username already exists
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = :username");
+    $stmt->execute(['username' => $username]);
+
+    if ($stmt->rowCount() > 0) {
+        echo json_encode(['status' => 'error', 'message' => 'Username already exists']);
+        exit;
+    }
+
+    // Hash the password
+    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+    // Insert the new user into the database
+    $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
+    $stmt->execute(['username' => $username, 'password' => $hashedPassword]);
+
+    echo json_encode(['status' => 'success', 'message' => 'User registered successfully']);
+} else {
+    echo json_encode(['status' => 'error', 'message' => 'Invalid request method']);
+}
+?>
